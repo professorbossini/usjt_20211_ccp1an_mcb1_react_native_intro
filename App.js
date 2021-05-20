42a750b159bdf28a8b3ba48b6116e602ffa6bf29
Professor Rodrigo Bossini <professorbossini@gmail.com>
2021-05-20 20:15:55 -0300

    Terminamos a apostila 02

--- App.js
+++ App.js
@@ -1,15 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      contador: 0
+    }
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.contador}</Text>
+        <View style={{marginTop: 12}}>
+          <Button 
+            title='Alterar texto'
+            onPress={() => this.setState({ contador: this.state.contador + 1})}
+          />
+        </View>
+      </View>
+    );
+  }
 }
+
+// export default function App() {
+//   //hook
+//   const [texto, setTexto] = useState('Texto Inicial');
+//   return (
+
+//     <View style={styles.container}>
+//       <Text>{texto}</Text>
+//       <View style={{marginTop: 12}}>
+//         <Button 
+//           title="Alterar texto"
+//           onPress={() => setTexto("Novo Texto")}
+//         />
+//       </View>
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
